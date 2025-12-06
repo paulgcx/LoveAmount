a6c361ac56ffd8d370d4082dd115c4c612b6527e
MrPaul <its.mrpaul57@gmail.com>
2025-12-06 22:01:56 +0100

    Icono añadido

--- resources/views/love.blade.php
+++ resources/views/love.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>❤️</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/love.css') }}">
     <link rel="stylesheet" href="{{ asset('css/arrow.css') }}">
 </head>
