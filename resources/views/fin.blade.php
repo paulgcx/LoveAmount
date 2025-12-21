9cb1de4f09984bce4fe0f05f78876835f1b794a9
MrPaul <its.mrpaul57@gmail.com>
2025-12-21 13:52:01 +0100

    Fin

---
+++ resources/views/fin.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @vite(['resources/sass/love.scss'])
+</head>
+<body>
+    <div class="container">
+        Lo siento por arruinarlo todo
+    </div>
+</body>
+</html>
